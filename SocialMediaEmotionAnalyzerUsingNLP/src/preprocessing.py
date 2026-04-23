0b4d23a78b3d3cf2c91241efa74d79c92d0afc22
KiranAslam <kiranaslam028@gmail.com>
2026-04-23 22:55:55 +0500

    Update preprocessing.py

--- SocialMediaEmotionAnalyzerUsingNLP/src/preprocessing.py
+++ SocialMediaEmotionAnalyzerUsingNLP/src/preprocessing.py
@@ -3,10 +3,12 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+    nltk.download('wordnet')
+    nltk.download('omw-1.4')
 
 lemmatizer = WordNetLemmatizer()
 base_stop_words = set(stopwords.words('english'))
